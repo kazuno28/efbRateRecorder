88ead8aa052f458014e17d882da1fcca9b8a5cf3
kazuno28 <kazuki.soccer1013.no28@gmail.com>
2021-09-22 13:53:06 +0000

    コメント途中

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,14 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @rate_graph_comment = RateGraphComment.find(params[:id])
+    @rate_graph_comment = RateGraphComment.find(params[:rate_graph_comment_id])
     favorite = @rate_graph_comment.favorites.new(user_id: current_user.id)
     favorite.save
   end
 
   def destroy
-    @rate_graph_comment = RateGraphComment.find(params[:id])
-    favorite = @rate_graph_comment.favorites.new(user_id: current_user.id)
+    @rate_graph_comment = RateGraphComment.find(params[:rate_graph_comment_id])
+    favorite = @rate_graph_comment.favorites.find_by(user_id: current_user.id)
     favorite.destroy
   end
 end
--- app/controllers/rate_graph_comments_controller.rb
+++ app/controllers/rate_graph_comments_controller.rb
@@ -2,10 +2,19 @@ class RateGraphCommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @user = User.find(params[:user_id])
+    @rate_graph_comment = RateGraphComment.new(rate_graph_comment_params)
+    @rate_graph_comment.user_id = current_user.id
+    unless @rate_graph_comment.save
+      render 'error'
+    end
     
   end
 
   def destroy
+    @user = User.find(params[:user_id])
+    rate_graph_comment = @user.rate_graph_comments.find(params[:id])
+    rate_graph_comment.destroy
   end
   
   private
